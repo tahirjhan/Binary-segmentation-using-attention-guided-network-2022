646c2a3ca9c8d9e6f7ae81ef0bb8e2b2ab80862f
Tahir Mahmood <49135268+tahirjhan@users.noreply.github.com>
2022-11-17 12:16:33 +0900

    initial commit

---
+++ nets/DDNet.py
@@ -0,0 +1,206 @@
+
+from torch import nn
+import torchvision
+import torch
+import torch.nn.functional as F
+from nets.utils import TrippleConv, multi_scale_aspp, MHSA
+#------------------------------------------------------------------------------
+
+class backbone(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        baselayers = torchvision.models.densenet201(pretrained=True, progress=True)
+        self.custom_model = nn.Sequential(*list(baselayers.features.children())[:-6])
+        #print(self.custom_model)
+
+    def forward(self, input):
+        output = self.custom_model(input)
+        return output
+#------------------------------------------------
+
+#128, 32, 32
+class encoder_block(nn.Module):
+    def __init__(self, in_c, out_c):
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(in_channels=in_c, out_channels=out_c, kernel_size=(3, 3), stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(out_c)
+        self.conv2 = nn.Conv2d(in_channels=out_c, out_channels=out_c, kernel_size=(3, 3), stride=2, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_c)
+        self.relu = nn.ReLU()
+
+    def forward(self, inputs):
+        x = self.conv1(inputs)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+
+        return x
+#---------------------------------------------
+class decoder_block(nn.Module):
+    def __init__(self, in_c, out_c):
+        super().__init__()
+
+        self.up = nn.ConvTranspose2d(in_c, out_c, kernel_size=2, stride=2, padding=0)
+        self.conv = conv_block(out_c, out_c)
+
+    def forward(self, inputs):
+        up = self.up(inputs)  # 256,16,16
+        x =self.conv(up)
+
+
+        #x = torch.cat([x, skip], axis=1)
+        #x = self.conv(x)
+        return x
+#------------------------------------------
+class conv_block(nn.Module):
+    def __init__(self, in_c, out_c):
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(in_c, out_c, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(out_c)
+
+        self.conv2 = nn.Conv2d(out_c, out_c, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_c)
+
+        self.relu = nn.ReLU()
+
+    def forward(self, inputs):
+        x = self.conv1(inputs)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+
+        return x
+###########################################
+
+class DDNet(nn.Module):
+    def __init__(self):
+        super(DDNet, self).__init__()
+
+        # Encoder
+        self.block1 = backbone() # [ 128, 32, 32] if one block
+
+        # Stream 1
+        self.e1 = encoder_block(128,256) # 256, 16, 16
+        self.e2 = encoder_block(256, 512) # 512, 8, 8
+        self.e3 = encoder_block(512, 1024) # 1024, 4, 4
+
+        # Stream 2
+
+
+        # Decoder
+        self.d1 = decoder_block(1024,512)  # 512, 8, 8
+        self.d2 = decoder_block(512, 256)  # 256, 16, 16
+        self.d3 = decoder_block(256, 128)  # 128, 32, 32
+        self.d4 = decoder_block(128, 64)   # 64, 64, 64
+        self.d5 = decoder_block(64, 32)    # 32, 128, 128
+        self.d6 = decoder_block(32, 16)    # 16, 256, 256
+
+        # Classification
+        self.outc = nn.Conv2d(16, 1, kernel_size=1, padding=0)
+        self.last_activation = nn.Sigmoid()
+
+    def forward(self, input):
+            # Encoder
+        s1 = self.block1(input)  # 128, 32, 32
+
+        # stream 1
+        e1 = self.e1(s1)
+        e2 = self.e2(e1)
+        encoder_out = self.e3(e2)
+        #
+        # # Decoder
+        d1 = self.d1(encoder_out)
+        d2 = self.d2(d1)
+        d3 = self.d3(d2)
+        d4 = self.d4(d3)
+        d5 = self.d5(d4)
+        d6 = self.d6(d5)
+
+        if self.last_activation is not None:
+            logits = self.last_activation(self.outc(d6))
+
+            return logits
+
+model = DDNet()
+input = torch.randn((1,3,256,256))
+output = model(input)
+#print (output.shape)
+
+    #     # ******************** Encoding image ********************
+    #     #originalmodel = torchvision.models.vgg16(pretrained=True, progress=True)
+    #     #self.custom_model = nn.Sequential(*list(originalmodel.features.children())[:-21])
+    #
+    #     originalmodel = torchvision.models.densenet169(pretrained=False, progress=True)
+    #     # pretrained_model = models.vgg16(pretrained=True).features
+    #     self.custom_model = nn.Sequential(*list(originalmodel.features.children())[:-5])
+    #     self.layer1 = nn.Sequential(
+    #         nn.Conv2d(in_channels=512, out_channels=256, kernel_size=(3, 3), stride=1, padding=1),
+    #     )
+    #
+    #     self.deform1 = DeformConv2d(256, 128, 3, padding=1, modulation=True)
+    #     self.deform2 = DeformConv2d(128, 128, 3, padding=1, modulation=True)
+    #     # nn.ReLU(),
+    #     self.deform3 = DeformConv2d(256, 64, 3, padding=1, modulation=True)
+    #
+    #     # ******************** Decoding image ********************
+    #     self.deconv1 = nn.ConvTranspose2d(in_channels=64, out_channels=64, kernel_size=(3, 3), stride=2, padding=1)
+    #     self.deconv2 = nn.ConvTranspose2d(in_channels=64, out_channels=128, kernel_size=(3, 3), stride=2, padding=1)
+    #     #self.deform = DeformConv2d(256, 128, 3, padding=1, modulation=True)
+    #     self.upsampling1 = nn.Upsample(size=(128, 128), mode='bilinear', align_corners=True)
+    #
+    #     self.deconv3 = nn.ConvTranspose2d(in_channels=128, out_channels=1, kernel_size=(3, 3), stride=2, padding=1)
+    #     self.upsampling2 = nn.Upsample(size=(224, 224), mode='bilinear', align_corners=True)
+    #     self.outc = nn.Conv2d(128, 1, kernel_size=(1, 1))
+    #     self.last_activation = nn.Sigmoid()
+    #
+    #
+    #
+    # def forward(self, x):
+    #     # ******************** Initializing filters ********************
+    #     # h_filter = torch.tensor([[-1., 0., 1.],
+    #     #                 [-2., 1., 2.],
+    #     #                 [-1., 0., 1.]]).to('cuda')
+    #     # h_filter = h_filter.view(1, 1, 3, 3).repeat(256, 256, 1, 1) # convolution mask (gx)
+    #     #
+    #     # v_filter = torch.tensor([[-1., -2., -1.],
+    #     #                          [0., 0., 0.],
+    #     #                          [1., 2., 1.]]).to('cuda')
+    #     # v_filter = v_filter.view(1, 1, 3, 3).repeat(1, 1, 1, 1) # convolution mask (gy)
+    #     # ******************** Encoding image ********************
+    #     x = self.custom_model(x)
+    #     x = self.layer1(x)
+    #
+    #     deform1_x = self.deform1(x)
+    #     deform2_x = self.deform2(deform1_x)
+    #     x =  torch.cat((deform1_x,deform2_x),1)
+    #     x = self.deform3(x)
+    #     # x = self.deform3(deform2_x)
+    #
+    #
+    #
+    #     # # ******************** Decoding image ********************
+    #     x = self.deconv1(x)
+    #     x = self.deconv2(x)
+    #     # x = self.deform(x)
+    #     # x = F.conv2d(x, h_filter)
+    #
+    #     x = self.upsampling1(x)
+    #     x = self.deconv3(x)
+    #     # x = F.conv2d(x, v_filter)
+    #     x = self.upsampling2(x)
+    #     if self.last_activation is not None:
+    #         logits = self.last_activation(self.outc(x))
+    #         # print("111")
+    #     else:
+    #         logits = self.outc(x)
+    #
+    #     return logits
---
+++ nets/utils.py
@@ -0,0 +1,134 @@
+from torch import nn
+import torch
+import torch.nn.functional as F
+
+class TrippleConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(TrippleConv, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels,1)
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+
+        self.conv2 = nn.Conv2d(out_channels, out_channels,1)
+
+    def forward(self, x):
+        y = self.relu(self.bn(self.conv1(x)))
+        y = self.relu(self.bn(self.conv2(y)))
+
+        return y
+
+class MHSA(nn.Module):
+    def __init__(self, n_dims, width=14, height=14, heads=4):
+        super(MHSA, self).__init__()
+        self.heads = heads
+
+        self.query = nn.Conv2d(n_dims, n_dims, kernel_size=1)
+        self.key = nn.Conv2d(n_dims, n_dims, kernel_size=1)
+        self.value = nn.Conv2d(n_dims, n_dims, kernel_size=1)
+
+        self.rel_h = nn.Parameter(torch.randn([1, heads, n_dims // heads, 1, height]), requires_grad=True)
+        self.rel_w = nn.Parameter(torch.randn([1, heads, n_dims // heads, width, 1]), requires_grad=True)
+
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, x):
+        n_batch, C, width, height = x.size()
+        q = self.query(x).view(n_batch, self.heads, C // self.heads, -1)
+        k = self.key(x).view(n_batch, self.heads, C // self.heads, -1)
+        v = self.value(x).view(n_batch, self.heads, C // self.heads, -1)
+
+        content_content = torch.matmul(q.permute(0, 1, 3, 2), k)
+
+        content_position = (self.rel_h + self.rel_w).view(1, self.heads, C // self.heads, -1).permute(0, 1, 3, 2)
+        content_position = torch.matmul(content_position, q)
+
+        energy = content_content + content_position
+        attention = self.softmax(energy)
+
+        out = torch.matmul(v, attention.permute(0, 1, 3, 2))
+        out = out.view(n_batch, C, width, height)
+
+        return out
+
+class _DenseAsppBlock(nn.Sequential):
+    """ ConvNet block for building DenseASPP. """
+
+    def __init__(self, input_num, num1, num2, dilation_rate, drop_out, bn_start=True):
+        super(_DenseAsppBlock, self).__init__()
+        self.asppconv = torch.nn.Sequential()
+        if bn_start:
+            self.asppconv = nn.Sequential(
+                nn.BatchNorm2d(input_num),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(in_channels=input_num, out_channels=num1, kernel_size=1),
+                nn.BatchNorm2d(num1),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(in_channels=num1, out_channels=num2, kernel_size=3,
+                          dilation=dilation_rate, padding=dilation_rate)
+            )
+        else:
+            self.asppconv = nn.Sequential(
+                nn.ReLU(inplace=True),
+                nn.Conv2d(in_channels=input_num, out_channels=num1, kernel_size=1),
+                nn.BatchNorm2d(num1),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(in_channels=num1, out_channels=num2, kernel_size=3,
+                          dilation=dilation_rate, padding=dilation_rate)
+            )
+        self.drop_rate = drop_out
+
+    def forward(self, _input):
+        #feature = super(_DenseAsppBlock, self).forward(_input)
+        feature = self.asppconv(_input)
+
+        if self.drop_rate > 0:
+            feature = F.dropout2d(feature, p=self.drop_rate, training=self.training)
+
+        return feature
+
+
+class multi_scale_aspp(nn.Sequential):
+    """ ConvNet block for building DenseASPP. """
+
+    def __init__(self, channel):
+        super(multi_scale_aspp, self).__init__()
+        self.ASPP_3 = _DenseAsppBlock(input_num=channel, num1=channel * 2, num2=channel, dilation_rate=3,
+                                      drop_out=0.1, bn_start=False)
+
+        self.ASPP_6 = _DenseAsppBlock(input_num=channel * 2, num1=channel * 2, num2=channel,
+                                      dilation_rate=6, drop_out=0.1, bn_start=True)
+
+        self.ASPP_12 = _DenseAsppBlock(input_num=channel * 3, num1=channel * 2, num2=channel,
+                                       dilation_rate=12, drop_out=0.1, bn_start=True)
+
+        self.ASPP_18 = _DenseAsppBlock(input_num=channel * 4, num1=channel * 2, num2=channel,
+                                       dilation_rate=18, drop_out=0.1, bn_start=True)
+
+        self.ASPP_24 = _DenseAsppBlock(input_num=channel * 5, num1=channel * 2, num2=channel,
+                                       dilation_rate=24, drop_out=0.1, bn_start=True)
+        self.classification = nn.Sequential(
+            nn.Dropout2d(p=0.1),
+            nn.Conv2d(in_channels=channel * 6, out_channels=channel, kernel_size=1, padding=0)
+        )
+
+    def forward(self, _input):
+        #feature = super(_DenseAsppBlock, self).forward(_input)
+        aspp3 = self.ASPP_3(_input)
+        feature = torch.cat((aspp3, _input), dim=1)
+
+        aspp6 = self.ASPP_6(feature)
+        feature = torch.cat((aspp6, feature), dim=1)
+
+        aspp12 = self.ASPP_12(feature)
+        feature = torch.cat((aspp12, feature), dim=1)
+
+        aspp18 = self.ASPP_18(feature)
+        feature = torch.cat((aspp18, feature), dim=1)
+
+        aspp24 = self.ASPP_24(feature)
+
+        feature = torch.cat((aspp24, feature), dim=1)
+
+        aspp_feat = self.classification(feature)
+
+        return aspp_feat
